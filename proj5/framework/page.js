6f1aeaffbf63feca68b96d63d414cbc8005243cc
Bharat Jograna <bharat@jbspl.com>
2018-12-20 17:38:10 +0530

    OOP-es6 final demo
    car, drone, error, validation, sorting, button, image, title-bar, map, onClick event.

---
+++ proj5/framework/page.js
@@ -0,0 +1,8 @@
+import { BaseElement } from "../src/ui/base-element.js";
+
+export class Page extends BaseElement {
+    constructor(pageTitle) {
+        super();
+        this.pageTitle = pageTitle;
+    }
+}
